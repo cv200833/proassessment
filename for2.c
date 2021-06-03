6d257e41d5ba68d8957368ef2af495029cdb0a9f
cv200833 <85276188+cv200833@users.noreply.github.com>
2021-06-03 15:53:22 +0900

    Update for2.c

--- for2.c
+++ for2.c
@@ -3,10 +3,10 @@
 /* 
 int main()
 {
-	int i; 
-	for (i = 1; i < 10; i++)
+	int i;  //정수 i 저장
+	for (i = 1; i < 10; i++) //i=1, i가 10보다 작으면 1씩 더함
 	{
-		printf("2x%d = %2d\n", i, i * 2); 
+		printf("2x%d = %2d\n", i, i * 2);  //문장 출력
 	}
 	return 0; 
 }
@@ -15,13 +15,12 @@ int main()
 #include <stdio.h>
 int main()
 {
-	int i, j; 
-	for(i = 2; i < 10; i++)
+	int i, j;  //정수 i,j 저장
+	for(i = 2; i < 10; i++) //i는 2, i가 10보다 작으면 1씩 더함
 	{
-		for (j = 1; j < 10; j++)
+		for (j = 1; j < 10; j++) //j=2, j가 10보다 작으면 1씩 더함
 		{
-			printf("%dx%d = %d", i, j, i*j); 
+			printf("%dx%d = %d", i, j, i*j);  //문장 출력
 		}
 	}
 }
-© 2021 GitHub, Inc.
