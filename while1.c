0721f9e7bfafd689e746acc30dfc81206c30e13b
cv200833 <85276188+cv200833@users.noreply.github.com>
2021-06-03 15:49:50 +0900

    Update while1.c

--- while1.c
+++ while1.c
@@ -2,11 +2,11 @@
 int main()
 {
 	int i = 1, end;
-	printf("수를 입력하세요: "); 
-	scanf("%d", &end); 
-	while (i < end); { 
-	printf("%d", i); 
-	i++; 
+	printf("수를 입력하세요: ");  //문장 출력
+	scanf("%d", &end);  //d값 저장
+	while (i < end); {  //i가 end보다 작을때까지 반복
+	printf("%d", i);  //d값, i 출력
+	i++; //먼저 해당 연산을 수행하고 나서, 피연산자의 값을 1 증가시킴
 	}
 	return 0;
 }
